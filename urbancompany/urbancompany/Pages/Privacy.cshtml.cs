4821fa63661e9bed6880a60f287a6e933c5de73a
keerthi <munny.1482477@gmail.com>
2026-07-20 18:22:55 +0530

    added .gitignore

---
+++ urbancompany/urbancompany/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace urbancompany.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
